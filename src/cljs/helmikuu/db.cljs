bb927adeac80b1194c1776622de727d52a93fcc1
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-05-28 17:09:00 +0300

    edited slug url in config

--- src/cljs/helmikuu/db.cljs
+++ src/cljs/helmikuu/db.cljs
@@ -9,10 +9,9 @@
 (def wp-url (str (:wordpress-post-url config)))
 
 (defn slug-url [slug]
-  (str (:wordpress-post-url config) "/slug:" slug))
+  (str (:wordpress-slug-url config) slug))
 
 (defn get-blog-posts []
-  (apply js/console.log config2)
   (http/get wp-url {:with-credentials? false}))
 
 (defn get-one-post [slug]
