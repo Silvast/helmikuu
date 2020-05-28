8da9fa23421cbea1e07212bf925e3900a17ca69b
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-05-28 17:09:36 +0300

    edited config

--- src/cljs/helmikuu/conf.cljs
+++ src/cljs/helmikuu/conf.cljs
@@ -1,9 +1,7 @@
 (ns helmikuu.conf)
 
 (def config
-  {:wordpress-host
-   "http://95.217.17.177:8000"
-   :wordpress-post-url
-   "https://public-api.wordpress.com/rest/v1.1/sites/anskufail.wordpress.com/posts"})
-
-  ; "https://public-api.wordpress.com/rest/v1.1/sites/anskufail.wordpress.com"})
+  {:wordpress-post-url
+   "https://public-api.wordpress.com/rest/v1.1/sites/anskufail.wordpress.com/posts"
+   :wordpress-slug-url 
+   "https://public-api.wordpress.com/rest/v1.1/sites/anskufail.wordpress.com/posts/slug:"})
