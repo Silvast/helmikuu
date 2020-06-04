402f2e199fb9a6853466135f6dd5fc3a49b91182
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-06-04 17:07:30 +0300

    edited routes to include correct dispatching

--- src/cljs/helmikuu/routes.cljs
+++ src/cljs/helmikuu/routes.cljs
@@ -6,8 +6,7 @@
    [goog.events :as gevents]
    [goog.history.EventType :as EventType]
    [re-frame.core :as re-frame]
-   [helmikuu.events :as events]
-   [helmikuu.handlers :as handlers]))
+   [helmikuu.events :as events]))
 
 (defn hook-browser-navigation! []
   (doto (History.)
@@ -22,18 +21,21 @@
   ;; --------------------
   ;; define routes here
   (defroute "/" []
-    (re-frame/dispatch [::events/set-active-panel :home-panel])
-    )
+    (re-frame/dispatch [::events/set-active-panel :home-panel]))
 
   (defroute "/about" []
     (re-frame/dispatch [::events/set-active-panel :about-panel]))
 
 (defroute "/blog" []
-  (re-frame/dispatch [::events/set-active-panel :blog-panel]))
+  (do
+    (re-frame/dispatch [::events/get-all-posts])
+    (re-frame/dispatch [::events/set-active-panel :blog-panel])))
 
 (defroute "/blog/:slug" [slug]
-  (re-frame/dispatch [::events/set-slug slug])
-  (re-frame/dispatch [::events/set-active-panel :blogitem-panel]))
+  (do
+    (re-frame/dispatch [::events/set-slug slug])
+    (re-frame/dispatch [::events/get-blogpost slug])
+    (re-frame/dispatch [::events/set-active-panel :blogitem-panel])))
 
   ;; --------------------
   (hook-browser-navigation!))
