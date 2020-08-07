b0d9f4074a50cff6ed02947c193ebc1e251aceb4
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-08-07 16:04:08 +0300

    tried nginx-certbot

--- resources/scripts/deploy.sh
+++ resources/scripts/deploy.sh
@@ -1,7 +1,7 @@
-cd ~/helmikuu/resources
+cd ~/helmikuu/resources/configuration/nginx/
 docker stop helmikuu || true && docker rm helmikuu || true
 docker build --no-cache -t helmikuuimage .
-docker run -d --rm --name helmikuu -p 80:80 helmikuuimage
+docker run -d --rm -v ~/letsencrypt:/etc/letsencrypt --name helmikuu -p 80:80 helmikuuimage
 #cd ~/helmikuu/resources/configuration/nginx
 #docker build -t nginx-certbot .
 #docker run -v $(pwd)/letsencrypt:/etc/letsencrypt --name nginx -ti -p 8080:80 nginx-cert sh
