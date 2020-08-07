66349298a16c952655a60684df5cb8cda440780c
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-08-07 10:38:35 +0300

    added deployscript to run docker

--- resources/scripts/deploy.sh
+++ resources/scripts/deploy.sh
@@ -1,6 +1,4 @@
-# docker stop helmikuu
-# docker rm helmikuu
-# cd ~/helmikuu/
-# docker run -d --name helmikuu helmikuuimage
-
-echo "Hello world" > test.txt
+cd ~/helmikuu/resources
+docker stop helmikuu || true && docker rm helmikuu || true
+docker build --no-cache -t helmikuuimage .
+docker run -d --rm --name helmikuu -p 8080:8080 helmikuuimage
