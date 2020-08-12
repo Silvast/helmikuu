14d7998f713ca74bce9bf122b2f393ad82de81ae
Anne-Mari Silvast <anne-mari.silvast@gofore.com>
2020-08-12 15:12:11 +0300

    added missing routes to english section

--- src/cljs/helmikuu/routes.cljs
+++ src/cljs/helmikuu/routes.cljs
@@ -31,17 +31,23 @@
       (re-frame/dispatch [::events/get-blogpost "about"])
       (re-frame/dispatch [::events/set-active-panel :about-panel])))
 
-(defroute "/blog" []
-  (do
-    (re-frame/dispatch [::events/get-all-posts])
-    (re-frame/dispatch [::events/set-active-panel :blog-panel])))
+  (defroute "/english" []
+    (do
+      (re-frame/dispatch [::events/set-slug "english"])
+      (re-frame/dispatch [::events/get-blogpost "english"])
+      (re-frame/dispatch [::events/set-active-panel :english-panel])))
 
-(defroute "/blog/:slug" [slug]
-  (do
-    (re-frame/dispatch [::events/set-slug slug])
-    (re-frame/dispatch [::events/get-blogpost slug])
-    (re-frame/dispatch [::events/set-active-panel :blogitem-panel])
-    (re-frame/dispatch [::events/set-title])))
+  (defroute "/blog" []
+    (do
+      (re-frame/dispatch [::events/get-all-posts])
+      (re-frame/dispatch [::events/set-active-panel :blog-panel])))
+
+  (defroute "/blog/:slug" [slug]
+    (do
+      (re-frame/dispatch [::events/set-slug slug])
+      (re-frame/dispatch [::events/get-blogpost slug])
+      (re-frame/dispatch [::events/set-active-panel :blogitem-panel])
+      (re-frame/dispatch [::events/set-title])))
 
   ;; --------------------
   (hook-browser-navigation!))
